41cf3ddb31c1b0bc68764e161c8feacc704876c7
Preeti-Pillai <49134208+PREETIPILLAI1@users.noreply.github.com>
2019-04-06 22:44:56 -0500

    Updated line 257

--- bikeshare.py
+++ bikeshare.py
@@ -254,7 +254,7 @@ def main():
         user_stats(df)
         display_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart all over? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
